aa108c34752a287967b3d2663f6e79411fe646d2
srouaix <sebastien.rouaix@gmail.com>
2017-04-04 11:15:45 +0200

    correction 2

--- HelloIppon.java
+++ HelloIppon.java
@@ -3,7 +3,7 @@ public class HelloIppon
 	public static void main (String [] args)
 	{
 		System.out.println("  _______  _______  ______       _______  _______           _______ _________         ");
-		System.out.println(" (  ____ \\(  ____ \\(  ___ \\     (  ____ )(  ___  )|\\     /|(  ___  )\\__   __/|\     /|");
+		System.out.println(" (  ____ \\(  ____ \\(  ___ \\     (  ____ )(  ___  )|\\     /|(  ___  )\\__   __/|\\     /|");
 		System.out.println(" | (    \\/| (    \\/| (   ) )    | (    )|| (   ) || )   ( || (   ) |   ) (   ( \\   / )");
 		System.out.println(" | (_____ | (__    | (__/ /     | (____)|| |   | || |   | || (___) |   | |    \\ (_) / ");
 		System.out.println(" (_____  )|  __)   |  __ (      |     __)| |   | || |   | ||  ___  |   | |     ) _ (  ");
